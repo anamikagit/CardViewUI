fa18b4820ba981b4d399af49b9ce660e1188cf9f
anamikagit <anamika.arya1990@gmail.com>
2018-05-04 16:50:00 +0530

    toolbar added successfuly

--- app/src/main/java/com/example/anamika/cardviewui/LoginActivity.java
+++ app/src/main/java/com/example/anamika/cardviewui/LoginActivity.java
@@ -4,10 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 public class LoginActivity extends AppCompatActivity {
 EditText edtPwd;
 TextView textView;
+Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +17,9 @@ TextView textView;
 
         edtPwd = findViewById(R.id.edtPwd);
         textView = findViewById(R.id.tvLoginId);
+        toolbar = findViewById(R.id.toolbar);
+      //toolbar.setTitle(R.string.app_name);
+//        toolbar.setTitle("Guard Monitor");
+       
     }
 }
