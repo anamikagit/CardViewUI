8f9592528071a5e71a79d5b0f30eadbed99b1a6f
anamikagit <anamika.arya1990@gmail.com>
2018-05-10 16:56:13 +0530

    intent written but not working

--- app/src/main/java/com/example/anamika/cardviewui/LoginActivity.java
+++ app/src/main/java/com/example/anamika/cardviewui/LoginActivity.java
@@ -5,11 +5,14 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.content.Intent;
 
-public class LoginActivity extends AppCompatActivity {
+
+public class LoginActivity extends AppCompatActivity{
 
     @butterknife.BindView(R.id.edtPwd) EditText edtPwd;
     @butterknife.BindView(R.id.tvLoginId) TextView textView;
+    @butterknife.BindView(R.id.loginBtn) TextView loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +24,9 @@ public class LoginActivity extends AppCompatActivity {
         butterknife.ButterKnife.bind(this);
         setSupportActionBar(toolbar);
 
+        Intent i = new Intent(LoginActivity.this , MainActivity.class);
+
+       // android.content.Intent intent = new in
+       // android.content.Intent intent = new android.content.Intent(this, MainActivity.class);
     }
 }
